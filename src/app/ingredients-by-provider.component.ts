fe7a3412b769978b6e2cb3e2c4234e5304caeaea
Romain <rom.delav@gmail.com>
2021-04-13 16:41:01 -0500

    updated ingredients-by-provider component

--- src/app/ingredients-by-provider.component.ts
+++ src/app/ingredients-by-provider.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IngredientCRUDService, Ingredient } from './ingredientCRUD.service';
+import { ProviderCRUDService, Provider } from './providerCRUD.service';
 
 @Component({
     selector: 'ingredients-by-provider',
@@ -12,19 +13,28 @@ export class IngredientsByProviderComponent implements OnInit {
     
     User_ID: number;
     ingredients: Ingredient[];
+    providers: Provider[];
 
     constructor(
         private ingredientCRUD: IngredientCRUDService,
+        private providerCRUD: ProviderCRUDService,
         private route: ActivatedRoute
     ) {}
 
     ngOnInit() {
         this.User_ID = this.route.snapshot.params['user_ID']
-        this.getIngredientsByProv();
+        this.getIngredients();
+        this.getAllProviders();
+
     }
 
-    getIngredientsByProv() {
+    getIngredients() {
        this.ingredientCRUD.getIngredientsByProvider(this.User_ID)
             .subscribe(data => { this.ingredients = data });  
+    }
+
+    getAllProviders() {
+        this.providerCRUD.getProviders()
+            .subscribe(data => { this.providers = data});
     }
 }
