6563f5f89002bd202117196bba108a859efa805d
Romain <rom.delav@gmail.com>
2021-04-16 21:28:02 -0500

    updated article component to add ability to post comments

--- src/app/article.component.ts
+++ src/app/article.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleCRUDService, Article } from './articleCRUD.service';
+import { CommentCRUDService, Comment } from './commentCRUD.service';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'article-page',
@@ -12,20 +14,38 @@ export class ArticleComponent implements OnInit {
     
     article_ID: number;
     article: Article;
+    form: FormGroup;
 
     constructor(
         private articleCRUD: ArticleCRUDService,
+        private commentCRUD: CommentCRUDService,
         private route: ActivatedRoute,
     ) {}
 
     ngOnInit() {
         this.article_ID = this.route.snapshot.params['article_ID']
         this.getArticleByID();
+
+        this.form = new FormGroup({
+            Username: new FormControl('', Validators.compose([
+                Validators.required,
+                Validators.pattern('^[a-zA-Z \-\']+')
+            ])),
+            Text: new FormControl('', Validators.compose([
+                Validators.required,
+                Validators.pattern('^[a-zA-Z \-\']+')
+            ]))
+        });
     }
 
     getArticleByID() {
         this.articleCRUD.getArticle(this.article_ID)
         .subscribe(data => { this.article = data });
     }
-
+    
+    onSubmit(newComment) {
+        this.commentCRUD.addComment(this.article_ID, newComment)
+          .subscribe() 
+    }
+    
 }
