f53ad7984725f46374ecb6cc1cee4eb75744e826
Romain <rom.delav@gmail.com>
2021-04-29 16:57:05 -0500

    implemented back to top button

--- src/app/provider-list-page.component.ts
+++ src/app/provider-list-page.component.ts
@@ -25,4 +25,9 @@ export class ProviderListPageComponent implements OnInit {
         this.providerCRUD.getProviders()
             .subscribe(data => { this.providers = data});
     }
+
+    topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    } 
 }
