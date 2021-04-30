0d7a3f67b5417948b3831c65b1472c052cf97afb
Romain <rom.delav@gmail.com>
2021-04-30 17:36:07 -0500

    implemented back to top button

--- src/app/vegan-recipe-list.component.ts
+++ src/app/vegan-recipe-list.component.ts
@@ -23,4 +23,9 @@ export class VeganRecipeListComponent implements OnInit {
         this.recipeCRUD.getVeganRecipes()
             .subscribe(data => { this.recipes = data});
     }
+
+    topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    } 
 }
