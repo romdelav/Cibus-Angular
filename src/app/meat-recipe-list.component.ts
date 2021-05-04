c0202b18639a57dfb3d29a977284dea4022db512
Romain <rom.delav@gmail.com>
2021-05-03 22:01:27 -0500

    implemented back to top button

--- src/app/meat-recipe-list.component.ts
+++ src/app/meat-recipe-list.component.ts
@@ -23,4 +23,9 @@ export class MeatRecipeListComponent implements OnInit {
         this.recipeCRUD.getMeatRecipes()
             .subscribe(data => { this.recipes = data});
     }
+
+    topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    } 
 }
