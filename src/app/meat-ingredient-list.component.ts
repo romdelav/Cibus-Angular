ccdad6ce8c76c29f9c42f6944b4c09d59d2ec8c1
Romain <rom.delav@gmail.com>
2021-05-04 17:57:18 -0500

    implemented back-to-top button

--- src/app/meat-ingredient-list.component.ts
+++ src/app/meat-ingredient-list.component.ts
@@ -23,4 +23,9 @@ export class MeatIngredientListComponent implements OnInit {
         this.ingredientCRUD.getMeat()
             .subscribe(data => { this.ingredients = data});
     }
+
+    topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    } 
 }
